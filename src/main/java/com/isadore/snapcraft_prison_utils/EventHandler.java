bc7fd8aa9cb4858341cea3e8e7f5edfcceffe16d
remyrascal <37592530+remyrascal@users.noreply.github.com>
2021-06-09 02:44:46 -0400

    custom packet handler for detecting slice and oreseek

--- src/main/java/com/isadore/snapcraft_prison_utils/EventHandler.java
+++ src/main/java/com/isadore/snapcraft_prison_utils/EventHandler.java
@@ -2,11 +2,9 @@ package com.isadore.snapcraft_prison_utils;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
+import net.minecraft.network.NetworkManager;
 import net.minecraftforge.client.event.*;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class EventHandler {
 
@@ -19,27 +17,29 @@ public class EventHandler {
             KeyBinds.resetTimer();
     }
 
-
     @SubscribeEvent
     public void renderOverlay(RenderGameOverlayEvent.Post event) {
         if(!snapcraft_prison_utils.serverIsSnapcraft()) return;
         if(event.getType() == RenderGameOverlayEvent.ElementType.ALL && !mc.gameSettings.showDebugInfo) {
             MatrixStack matrix = event.getMatrixStack();
             GuiOverlay.renderSliceTimer(matrix);
+            GuiOverlay.renderOreSeekTimer(matrix);
         }
     }
 
+    private boolean loggedIn = false;
+
     @SubscribeEvent
-    public void onMessage(ClientChatReceivedEvent event) {
-        String messageContent = event.getMessage().getString();
-        Pattern eTokenPattern = Pattern.compile("^\\+5 tokens for mining 250 blocks! \\(Use tokens to /enchant\\)$");
-        Matcher eTokenMatcher = eTokenPattern.matcher(messageContent);
-        if(eTokenMatcher.find()) {
-            if(GuiOverlay.eTokenMessageCounter.secondsElapsed() > 5) GuiOverlay.eTokenMessageCounter.reset();
-            GuiOverlay.eTokenMessageCounter.increment();
-            if(GuiOverlay.eTokenMessageCounter.secondsElapsed() < 5 && GuiOverlay.eTokenMessageCounter.messageCount >= 6)
-                GuiOverlay.sliceTimerEnd = System.currentTimeMillis() + GuiOverlay.sliceTimeMS;
-        }
+    public void onLogin(ClientPlayerNetworkEvent.LoggedInEvent event) {
+        NetworkManager manager = event.getNetworkManager();
+        if(manager == null || loggedIn) return;
+        manager.channel().pipeline().addBefore("packet_handler", "listener", new PacketHandler());
+        loggedIn = true;
+    }
+
+    @SubscribeEvent
+    public void onLogout(ClientPlayerNetworkEvent.LoggedOutEvent event) {
+        loggedIn = false;
     }
 
     @SubscribeEvent
---
+++ src/main/java/com/isadore/snapcraft_prison_utils/PacketHandler.java
@@ -0,0 +1,99 @@
+package com.isadore.snapcraft_prison_utils;
+
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.IPacket;
+import net.minecraft.network.play.server.SWindowItemsPacket;
+
+import javax.annotation.Nonnull;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PacketHandler extends ChannelDuplexHandler {
+
+    private static final Minecraft mc = Minecraft.getInstance();
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object obj) throws Exception {
+        super.channelRead(ctx, obj);
+        if(obj instanceof IPacket) {
+            this.onPacket((IPacket<?>) obj, true);
+        }
+    }
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object obj, ChannelPromise promise) throws Exception {
+        super.write(ctx, obj, promise);
+        if(obj instanceof IPacket) {
+            this.onPacket((IPacket<?>) obj, false);
+        }
+    }
+
+    private void onPacket(@Nonnull IPacket<?> packet, boolean incoming) {
+        Method[] methods = this.getClass().getDeclaredMethods();
+        String packetName = packet.getClass().getTypeName();
+        for (Method m : methods) {
+            Type[] params = m.getGenericParameterTypes();
+            if(params.length > 0 && packetName.equals(params[0].getTypeName())) {
+                try {
+                    if(params.length > 1 && params[1].getTypeName().equals("boolean")) {
+                        m.invoke(this, packet, incoming);
+                    } else {
+                        m.invoke(this, packet);
+                    }
+                } catch (Exception e) {
+                    snapcraft_prison_utils.LOGGER.error("Failed to call method: {} for packet: {}", m.getName(), packetName);
+                }
+            }
+        }
+    }
+
+    Map<String, Integer> lastItemCounts = new HashMap<>();
+
+    public void onItemsUpdate(@Nonnull SWindowItemsPacket packet) {
+        Map<String, Integer> itemCounts = new HashMap<>();
+        for(ItemStack i : packet.getItemStacks()) {
+            String itemID = InventoryUtils.getItemID(i);
+            if(itemID == null) continue;
+            itemCounts.computeIfAbsent(itemID, k -> i.getCount());
+            itemCounts.computeIfPresent(itemID, (k, v) -> v + i.getCount());
+        }
+        int maxChange = 0;
+        int increasedItemTypes = 0;
+        int decreasedItemTypes = 0;
+        for (Map.Entry<String, Integer> i : lastItemCounts.entrySet()) {
+            Integer currentVal = itemCounts.get(i.getKey());
+            Integer lastVal = i.getValue();
+            if(currentVal == null) {
+                decreasedItemTypes++;
+            } else if(!currentVal.equals(lastVal)) {
+                if(currentVal > lastVal)
+                    increasedItemTypes++;
+                else
+                    decreasedItemTypes++;
+                int change = currentVal - lastVal;
+                if(change > maxChange)
+                    maxChange = change;
+            }
+        }
+        if(maxChange > 25 && packet.getWindowId() == 0 && mc.player != null) {
+            Map<String, Integer> itemEnchants = InventoryUtils.getItemEnchants(mc.player.getHeldItemMainhand());
+            if(itemEnchants != null) {
+                if(itemEnchants.get("OreSeeker") != null && increasedItemTypes == 1 && decreasedItemTypes == 0)
+                    GuiOverlay.oreSeekTimerEnd = System.currentTimeMillis() + GuiOverlay.oreSeekTimeMS;
+                if(itemEnchants.get("Slicing") != null)
+                    GuiOverlay.sliceTimerEnd = System.currentTimeMillis() + GuiOverlay.sliceTimeMS;
+            }
+        }
+        lastItemCounts = itemCounts;
+    }
+
+}
+
+
+
