426c0de3d48c7a82b14a1afb555be9d6f84f0870
remyrascal <37592530+remyrascal@users.noreply.github.com>
2021-06-09 02:46:47 -0400

    added logger

--- src/main/java/com/isadore/snapcraft_prison_utils/snapcraft_prison_utils.java
+++ src/main/java/com/isadore/snapcraft_prison_utils/snapcraft_prison_utils.java
@@ -6,11 +6,14 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(snapcraft_prison_utils.MODID)
 public class snapcraft_prison_utils
 {
     public static final String MODID = "snapcraft_prison_utils";
+    public static final Logger LOGGER = LogManager.getLogger();
 
     public snapcraft_prison_utils() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
