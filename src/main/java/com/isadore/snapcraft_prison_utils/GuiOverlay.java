b07d4f425a167672173dd938639624ca98289ca5
remyrascal <37592530+remyrascal@users.noreply.github.com>
2021-06-09 02:45:32 -0400

    draw ui without texture files, added oreseek timer

--- src/main/java/com/isadore/snapcraft_prison_utils/GuiOverlay.java
+++ src/main/java/com/isadore/snapcraft_prison_utils/GuiOverlay.java
@@ -2,7 +2,8 @@ package com.isadore.snapcraft_prison_utils;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.client.gui.AbstractGui;
+import net.minecraft.util.ColorHelper;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
@@ -12,65 +13,64 @@ public class GuiOverlay {
 
     private static final Minecraft mc = Minecraft.getInstance();
 
-    private static final ResourceLocation empty_bar_texture = new ResourceLocation(snapcraft_prison_utils.MODID, "textures/empty_bar.png");
-    private static final ResourceLocation timer_bar_texture = new ResourceLocation(snapcraft_prison_utils.MODID, "textures/timer_bar.png");
-
-    public static int screenWidth() {
-        return mc.getMainWindow().getScaledWidth();
-    }
+    public static int screenWidth() { return mc.getMainWindow().getScaledWidth(); }
     public static int screenHeight() { return mc.getMainWindow().getScaledHeight(); }
 
     public static final int sliceTimeMS = 7 * 60 * 1000;
     public static long sliceTimerEnd = 0; //System.currentTimeMillis() + sliceTimeMS;
-    public static final MessageCounter eTokenMessageCounter = new MessageCounter();
+    public static final double oreSeekTimeMS = 60 * 1000;
+    public static double oreSeekTimerEnd = 0;
+
+    private static void renderBackgroundBar(MatrixStack matrix, int y) {
+        int lightGrey = ColorHelper.PackedColor.packColor(255, 153,153,153);
+        int darkGrey = ColorHelper.PackedColor.packColor(255, 56, 56, 56);
+        AbstractGui.fill(matrix, screenWidth() - 75, y + 10, screenWidth(), y, lightGrey);
+        AbstractGui.fill(matrix, screenWidth() - 49, y + 9, screenWidth() - 1, y + 1, darkGrey);
+    }
+
+    private static void renderPercentBar(MatrixStack matrix, int y, double percent, int color, int outlineColor) {
+        if(Double.isNaN(percent) || percent == 0) return;
+        int widthToSubtract = (int) (48 * (1 - percent));
+        int maxX = (screenWidth() - widthToSubtract);
+        AbstractGui.fill(matrix, screenWidth() - 49, y + 8, maxX - 1, y, outlineColor);
+        if(widthToSubtract >= 47) return;
+        AbstractGui.fill(matrix, screenWidth() - 48, y + 7, maxX - 2, y + 1, color);
+    }
 
     public static void renderSliceTimer(MatrixStack matrix) {
-
-        long sliceTimeRemaining = sliceTimerEnd - System.currentTimeMillis();
+        double sliceTimeRemaining = sliceTimerEnd - System.currentTimeMillis();
         int sliceMinutesRemaining = (int) (sliceTimeRemaining / 60000);
         int sliceSecondsRemaining = (int) (sliceTimeRemaining - (sliceMinutesRemaining * 60000)) / 1000;
-
-        mc.getTextureManager().bindTexture(empty_bar_texture);
-        mc.ingameGUI.blit(matrix, screenWidth() - 76, 1, 0, 0, 75, 10);
-
+        GuiOverlay.renderBackgroundBar(matrix, 0);
         if(sliceTimeRemaining >= 0) {
-            mc.getTextureManager().bindTexture(timer_bar_texture);
-            mc.ingameGUI.blit(matrix, screenWidth() - 50, 2, 0, 0, (int) (48 * sliceTimeRemaining/sliceTimeMS), 10);
-            mc.fontRenderer.drawString(matrix, String.format("%d:%02d", sliceMinutesRemaining, sliceSecondsRemaining), screenWidth() - 72, 3, 0);
-            matrix.push();
-            matrix.scale(3f, 3f, 0);
-
-                if(sliceTimeRemaining/1000 == 5 && mc.ingameGUI.displayedTitle == null) {
+            int brightRed = ColorHelper.PackedColor.packColor(255, 158, 26, 38);
+            int darkRed = ColorHelper.PackedColor.packColor(255, 215, 0, 39);
+            GuiOverlay.renderPercentBar(matrix, 1, sliceTimeRemaining/sliceTimeMS, darkRed, brightRed);
+            mc.fontRenderer.drawString(matrix, String.format("%d:%02d", sliceMinutesRemaining, sliceSecondsRemaining), screenWidth() - 71, 1, 0);
+            if(sliceTimeRemaining/1000 <= 5 && mc.ingameGUI.displayedTitle == null) {
                 long window = mc.getMainWindow().getHandle();
                 GLFW.glfwRequestWindowAttention(window);
-                ITextComponent announcementText = new StringTextComponent(String.format("Slice Cooldown Ending In %d Seconds", sliceTimeRemaining/1000))
+                ITextComponent announcementText = new StringTextComponent(String.format("Slice Cooldown Ending In %d Seconds", (int) sliceTimeRemaining/1000))
                         .mergeStyle(TextFormatting.DARK_RED);
                 mc.ingameGUI.renderTitles(announcementText, null, 1, 6, 1);
             }
-            matrix.pop();
         } else {
-            mc.fontRenderer.drawString(matrix, "0:00", screenWidth() - 72, 2, 0);
+            mc.fontRenderer.drawString(matrix, "0:00", screenWidth() - 71, 1, 0);
         }
-
     }
 
-}
-
-class MessageCounter {
-    public long startTimeStamp = System.currentTimeMillis();
-    public int messageCount = 0;
-
-    public long secondsElapsed() {
-        return (System.currentTimeMillis() - startTimeStamp) / 1000;
-    }
-
-    public void reset() {
-        this.startTimeStamp = System.currentTimeMillis();
-        this.messageCount = 0;
-    }
-
-    public void increment() {
-        this.messageCount++;
+    public static void renderOreSeekTimer(MatrixStack matrix) {
+        double oreSeekTimeRemaining = oreSeekTimerEnd - System.currentTimeMillis();
+        int secondsRemaining = (int) oreSeekTimeRemaining / 1000;
+        GuiOverlay.renderBackgroundBar(matrix, 11);
+        if(oreSeekTimeRemaining > 0) {
+            int darkPink = ColorHelper.PackedColor.packColor(255, 130, 47, 100);
+            int lightPink = ColorHelper.PackedColor.packColor(255, 235, 89, 181);
+            GuiOverlay.renderPercentBar(matrix, 12, oreSeekTimeRemaining / oreSeekTimeMS,  darkPink, lightPink);
+            mc.fontRenderer.drawString(matrix, String.format("0:%02d", secondsRemaining), screenWidth() - 71, 12, 0);
+        } else {
+            mc.fontRenderer.drawString(matrix, "0:00", screenWidth() - 71, 12, 0);
+        }
     }
 
 }
